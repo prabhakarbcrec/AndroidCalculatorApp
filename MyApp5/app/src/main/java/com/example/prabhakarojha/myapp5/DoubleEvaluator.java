4e6e69eb3cfa1630e98cb99c016075207f2d6c99
prabhakarbcrec <47571774+prabhakarbcrec@users.noreply.github.com>
2019-04-22 23:52:59 +0530

    Add files via upload

---
+++ MyApp5/app/src/main/java/com/example/prabhakarojha/myapp5/DoubleEvaluator.java
@@ -0,0 +1,21 @@
+package com.example.prabhakarojha.myapp5;
+
+import java.security.Policy;
+import java.util.Iterator;
+
+/**
+ * Created by prabhakarojha on 09-08-2016.
+ */
+public class DoubleEvaluator {
+    private static Policy.Parameters defaultParameters;
+
+    public static Policy.Parameters getDefaultParameters() {
+        return defaultParameters;
+    }
+
+    public double evaluate(Object evaluationContext) {
+
+
+        return 0;
+    }
+}
---
+++ MyApp5/app/src/main/java/com/example/prabhakarojha/myapp5/Function.java
@@ -0,0 +1,11 @@
+package com.example.prabhakarojha.myapp5;
+
+/**
+ * Created by prabhakarojha on 09-08-2016.
+ */
+public class Function {
+    public Function(String sqrt, int i) {
+
+
+    }
+}
